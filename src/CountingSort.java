0e2a0a7aa9fe7c7030d9b5d50f901048f4badac1
shop_app <mabdelhameed420@gmail.com>
2022-11-22 17:37:34 +0200

    first commit

---
+++ src/CountingSort.java
@@ -0,0 +1,33 @@
+class CountingSort {
+    public static void countingSort(int[] array, int size) {
+
+        int[] output = new int[size + 1];
+        int max = 0;
+        for (int i = 0; i < size; i++) {
+            max = Math.max(max, array[i]);
+        }
+
+        int[] count = new int[max + 1];
+        for (int i = 0; i < max; i++) {
+            count[i] = 0;
+        }
+
+        for (int i = 0; i < size; i++) {
+            count[array[i]]++;
+        }
+
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = 0; i < size; i++) {
+            output[count[array[i]] - 1] = array[i];
+            count[array[i]]--;
+        }
+
+        for (int i = 0; i < size; i++) {
+            array[i] = output[i];
+        }
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] data = {4, 2, 2, 8, 3, 3, 1};
+        System.out.println("--array before counting sort--");
+        System.out.println(Arrays.toString(data));
+        CountingSort.countingSort(data, data.length);
+        System.out.println("--array after counting sort--");
+        System.out.println(Arrays.toString(data));
+
+
+    }
+}
